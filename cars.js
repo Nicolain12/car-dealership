80a4dd08eb8bbea6a48940ae6bb288ca108d4885
Nicolas Lain <lainnicolas1@gmail.com>
2022-09-08 11:06:31 -0300

    basic structure

---
+++ cars.js
@@ -0,0 +1,26 @@
+let cars = [
+    {
+        brand: "ford", 
+        model: "fiesta", 
+        price: 150000, 
+        km: 200, 
+        color: "azul", 
+        installments: 12, 
+        year: 2019, 
+        patent: "APL123", 
+        soldOut: false
+    },
+    {
+        brand: "toyota",
+        model: "corolla",
+        price: 100000,
+        km: 0,
+        color: "blanco",
+        installments: 14,
+        year: 2019,
+        patent: "JJK116",
+        soldOut: false
+    }
+]
+
+module.exports = cars
---
+++ concesionaria.js
@@ -0,0 +1,50 @@
+let cars = require('./cars')
+
+const concesionaria = {
+   cars: cars,
+   findCar: function(patent){
+    let carByPatent = cars.filter(value => value.patent == patent)
+    if(carByPatent.length === 1){return carByPatent[0]}
+    else{return null}
+ },
+
+ sellcar: function(patent){
+    let index = cars.indexOf(this.findcar(patent))
+    cars[index].soldOut = true
+ },
+ 
+ carsForSell: function(){
+    let carsForSell = cars.filter(value => value.soldOut == false)
+    return carsForSell
+ },
+
+newCars: function(){
+    let newCarsToSell = this.carsForSell()
+    let carsNew = newCarsToSell.filter(value => value.km < 100)
+    return carsNew
+},
+
+salesList: function(){
+      let salesList = cars.filter(value =>value.soldOut == true)
+      let list = salesList.map(value => value.price)
+      return list
+  },
+ 
+  salesTotal: function(){
+   return (this.salesList()).reduce((acum, plus)=>acum + plus, 0)
+  },
+ 
+  puedeComprar: function(car, person){
+   return ((car.price / car.installments) < person.abilityToPayInInstallments) && (car.price < person.fullPaymentCapacity)
+  },
+  
+  carsQuePuedeComprar: function(person){
+   let carsToSell = this.carsForSell()
+   let carsFilteredOut = carsToSell.filter(value => ((value.price / value.installments) < person.abilityToPayInInstallments) && (value.price < person.fullPaymentCapacity))
+   return carsFilteredOut
+  }
+  
+
+};
+
+
